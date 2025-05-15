ce4ee8f6aabd360173c985b1c4b54c8d100e8141
Eda Dural <edaadurall@gmail.com>
2025-05-15 22:28:15 +0300

    movie details

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -42,7 +42,7 @@ const _layout = () => {
           backgroundColor: "#0f0D23",
           borderRadius: 50,
           marginHorizontal: 20,
-          marginBottom: 36,
+          marginBottom: 60,
           height: 52,
           position: "absolute",
           overflow: "hidden",
--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,10 +1,106 @@
-import { View, Text } from "react-native";
+import { View, ScrollView, Image, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { icons } from "@/constants/icons";
+
+interface MovieInfoProps {
+  label: string;
+  value?: string | number | null;
+}
+
+const MovieInfo = ({ label, value }: MovieInfoProps) => {
+  return (
+    <View className="flex-col items-start justify-center mt-5">
+      <Text className="text-light-200 text-sm font-normal">{label}</Text>
+      <Text className="text-light-100 text-sm font-bold">{value || "N/A"}</Text>
+    </View>
+  );
+};
 
 const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
   return (
-    <View>
-      <Text>MovieDetails</Text>
+    <View className="flex-1 bg-primary">
+      <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500/${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+
+        <View className="flex-col items-start justify-center mt-5 px-5">
+          <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+          <View className="flex-row items-center gap-x-1 mt-2">
+            <Text className="text-light-200 text-sm">
+              {movie?.release_date?.split("-")[0]}
+            </Text>
+            <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
+          </View>
+
+          <View className="flex-row items-center gap-x-1 mt-2 bg-dark-100 rounded-md px-2 py-1">
+            <Image
+              source={icons.star}
+              className="size-4"
+              resizeMode="contain"
+            />
+            <Text className="text-white font-bold text-sm">
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+            <Text className="text-light-200 text-sm">
+              ({movie?.vote_count} votes)
+            </Text>
+          </View>
+
+          <MovieInfo label="Overview" value={movie?.overview} />
+
+          <MovieInfo
+            label="Genres"
+            value={movie?.genres?.map((g) => g.name).join(", ") || "N/A"}
+          />
+
+          <View className="flex flex-row justify-between w-1/2">
+            <MovieInfo
+              label="Budget"
+              value={`$${(movie?.budget ?? 0) / 1_000_000} million`}
+            />
+            <MovieInfo
+              label="Revenue"
+              value={`$${Math.round(
+                (movie?.revenue ?? 0) / 1_000_000
+              )} million`}
+            />
+          </View>
+
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies.map((c) => c.name).join(", ") || "N/A"
+            }
+          />
+        </View>
+      </ScrollView>
+
+      <TouchableOpacity
+        className="absolute bottom-14 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex-row items-center justify-center z-50"
+        onPress={router.back}
+      >
+        <Image
+          source={icons.arrow}
+          className="size-5 mr-1 mt-0.5 rotate-180"
+          tintColor={"#fff"}
+        />
+        <Text className="text-white text-base font-semibold">Go back</Text>
+      </TouchableOpacity>
     </View>
   );
 };
